8eabcef020359a24e7f208581b61908d7156c164
dbbarreira <dbbarreira@192.168.1.244>
2017-04-07 11:59:26 +0200

    7/4/2017

--- main.c
+++ main.c
@@ -43,13 +43,47 @@ int main(int argc, char** argv) {
         printf("%x != %x \n",~uc1,uc2);
     }*/
     
-    unsigned char uc1=0x01;
+    /*unsigned char uc1=0x01;
     signed char uc2=0x01;
     
     signed char sc3=(uc2<<-9);
     
     printf("%d, %d\n",(uc1<<7),(sc3<<7));
-    printf("%x, %x\n",(uc1<<7),(sc3<<7));
+    printf("%x, %x\n",(uc1<<7),(sc3<<7));*/
+    
+ 
+    /*printf("\n 0.25 =  %f", (float)0.25); 
+    printf("\n 0.25 =  %.50f", (float)0.25); 
+    
+    printf("\n 0.1 =  %f", (float)0.1); 
+    printf("\n 0.1 =  %.50f", (float)0.1); 
+    
+    printf("\n 123456789 =  %f", (float)123456789);
+    printf("\n 123456789 =  %.40f", (float)123456789); 
+    
+    float x;
+    for( x = 1; x>=0; x-=0.1)
+    {
+        printf("\n%.50f",x);
+    }*/
+    
+    //Primer entero que no es representable como float
+    
+    /*float x = 1;
+    float y = 0;
+    
+    while(x!=y){
+        
+        x+=1;
+        y+=1;
+    }
+    
+    printf("\n%f",x);*/
+    
+    /*int x = 20;
+    
+    printf("%d", (int)(x * 0.20));*/
+    
     
     return (EXIT_SUCCESS);
 }
